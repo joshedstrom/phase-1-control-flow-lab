329b67b23f4725ee176591ce2c7d08f6458bd116
Joshua Hedstrom <jhhedstrom@gmail.com>
2021-09-23 17:39:45 -0400

    commit tree bfc6ca9da2c886ed33f14abe7d33ed908210360d

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let answer;
+  if (feet <= 400) {
+    answer = 'This one is on me!';
+  } else if (feet > 2500) {
+    answer = 'No can do.';
+  } else if (feet > 2000) {
+    answer = 'I will gladly take your thirty bucks.';
+  }
+  return answer;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  function ternaryCheckCity(city){
+    if (city === 'NYC') {
+      return 'Ok, sounds good.'
+    } else {
+      return 'No go.'
+    }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous') {
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
